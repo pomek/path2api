d9adc4f725c8445b8ca65cfecfcb77edcc661885
Kamil Piechaczek <pomek@desmart.com>
2015-04-04 17:04:55 +0200

    Service Provider Init

--- src/Path2API/Path2ApiServiceProvider.php
+++ src/Path2API/Path2ApiServiceProvider.php
@@ -4,14 +4,27 @@ namespace Pomek\Path2API;
 
 class Path2ApiServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/path2api.php';
+        $this->publishes([$configPath => config_path('path2api.php')], 'config');
+        $this->mergeConfigFrom($configPath, 'path2api');
+    }
+
     public function register()
     {
-        // TODO: Implement register() method.
+        $this->registerCommands();
+    }
+
+    protected function registerCommands()
+    {
+        $this->app->bindIf('command.path2api', function () {
+            return new GenerateDocsConsole($this->app['router'], $this->app['config'], $this->app['filesystem']);
+        });
+
+        $this->commands([
+            'command.path2api',
+        ]);
     }
 
 }
